b3b55cdae453f903eb9da43baa360238d5d42d9d
Pedro Jimenez <pedro.jimenez.2810@gmail.com>
2020-08-25 01:00:05 -0400

    Two-Way Data Binding

--- 01/app.js
+++ 01/app.js
@@ -6,6 +6,7 @@ new Vue({
 			symbol: 'BTC',
 			img: 'https://cryptologos.cc/logos/bitcoin-btc-logo.png',
 			changePercent: 10,
+			value: 0,
 			color: 'F4F4F4',
 			price: 8200,
 			pricesWithDays: [
@@ -25,6 +26,13 @@ new Vue({
 	computed: {
 		title(){
 			return `${this.name} - ${this.symbol}`;
+		},
+
+		convertedValue(){
+			if(!this.value){
+				return 0;
+			}
+			return this.value / this.price;
 		}
 	},
 
