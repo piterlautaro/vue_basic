b11e69116a22f70819ed042ea83a4fa8358748ea
Pedro Jimenez <pedro.jimenez.2810@gmail.com>
2020-08-24 23:57:59 -0400

    lists

--- 01/app.js
+++ 01/app.js
@@ -5,6 +5,16 @@ new Vue({
 			name: 'Bitcoin',
 			img: 'https://cryptologos.cc/logos/bitcoin-btc-logo.png',
 			changePercent: 10,
+			prices: [8400, 7900, 8200, 9000, 9100, 6000, 6300],
+			pricesWithDays: [
+				{day: 'Lunes', value: 8400},
+				{day: 'Martes', value: 7900},
+				{day: 'Miércoles', value: 8200},
+				{day: 'Jueves', value: 9000},
+				{day: 'Viernes', value: 9100},
+				{day: 'Sábado', value: 6000},
+				{day: 'Domingo', value: 6300},
+			]
 		}
 	}
 })
